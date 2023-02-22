4fed7ef686029186f254f01cd9c63939a2b77311
eleshinlomo <seun.olatunji2@gmail.com>
2023-02-22 18:41:39 -0500

    added padding

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -16,7 +16,7 @@ const Nav = () => {
 <nav className='nav'>
     
 <ul>
-<li><button onClick={()=>setMenu(true)}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="12" height="12"><path fill="none" d="M0 0h24v24H0z"/><path d="M3 4h18v2H3V4zm0 7h18v2H3v-2zm0 7h18v2H3v-2z"/></svg></button>
+<li><button className='nav-btn' onClick={()=>setMenu(true)}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="12" height="12"><path fill="none" d="M0 0h24v24H0z"/><path d="M3 4h18v2H3V4zm0 7h18v2H3v-2zm0 7h18v2H3v-2z"/></svg></button>
 {menu?
 <ul class="nav-list">
   <li ><a href='/'>Home</a></li>
