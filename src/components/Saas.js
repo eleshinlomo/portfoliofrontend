a14d27f52995ff1ff869fc2306a900cb4dc6ab89
eleshinlomo <seun.olatunji2@gmail.com>
2023-02-27 07:25:59 -0500

    email subscription

--- src/components/Saas.js
+++ src/components/Saas.js
@@ -9,39 +9,54 @@ const Saas = () => {
     
 
    const [saasModal, setSaasModal] = useState(false)
+   const [email, setEmail] = useState([])
+   const [message, setMessage] = useState()
 
-   const [data, setData] = useState([])
-   const handleChange = (e)=>{
-    setData(e.target.value)
-   }
-
-   const submit =(e)=>{
+   const emailSubmit =(e)=>{
     e.preventDefault()
-    fetch('http://localhost:3300/addSaasUsers', {
+    fetch('http://localhost:3300/api/addSaasUsers/',{
         method: "POST",
         mode: "cors",
-        body: {email: data}
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({email: email})
     })
-
-    .then((res)=>{
-        console.log(res)
+    .then((result)=>{
+        if(!result){
+            setMessage("There seem to be error with your request. Please check and resend")
+        }
+        setMessage(`Thank you for showing interest. We have received your email "${email}" and will be reaching out with details once the software launches.`)
         
     })
-}
+   }
+
+
 
 
 
   return (
     <div className='saas-main'>
     <div className='saas-card'>
-        <h2>ARE YOU A RECRUITER OR DO YOU WORK AS A TALENT AQUISITION PROFESSIONAL?</h2>
+       <div className='saas-card-text'>
+        <h3>ARE YOU A RECRUITER OR DO YOU WORK AS A TALENT AQUISITION PROFESSIONAL?</h3>
         <p>NEW SaaS LAUNCHING SOON. SOLUTION IS PACKED WITH ALL FEATURES TO MAKE YOUR TALENT FINDING PROCESS 100 TIMES EASIER.</p>
         <button onClick={()=>setSaasModal(true)}>TELL ME MORE</button>
+        </div>
+        <div className='saas-form'>
+        <form onSubmit={emailSubmit}>
+            <input onChange={(e)=>setEmail(e.target.value)} type="email" name="email" id="email" value={email} placeholder="Enter email for lifetime membership" required />
+            <button type='submit'>Submit</button>
+        </form>
+        </div>
+        <div className='saas-message'>
+        {message}
+        </div>
+    </div>
+        
         
         {saasModal?
             <div className='saas-modal'>
         <div className='saas-overlay'></div>
-        <ul className='saas-text'>
+        <ul className='saas-modal-text'>
             <li>Saas Features include:</li>
             <li>World class CMS to keep up with clients</li>
             <li>World class CMS to keep up with candidates</li>
@@ -50,16 +65,18 @@ const Saas = () => {
             <li>Manage Recruiters' performance within your team</li>
             <li>Ideal Software for every Staffing firm</li>
             <li>And much more...</li>
-            <p>Sign up for a lifetime membership...COMING SOON!</p>
+            <button className='saas-close-btn' onClick={()=>setSaasModal(false)}><a href="#email">Sign up for a lifetime membership</a></button>
+            
         </ul>
-        <button className='saas-close-btn' onClick={()=>setSaasModal(false)}>Close</button>
+        
+        
         </div>:null
         }
         
 
     </div>
          
-    </div>
+    
 
   )
 }
