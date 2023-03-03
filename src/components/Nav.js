886e34ef8fcf43ff40f1bcdb1bb2081a54ec511b
eleshinlomo <seun.olatunji2@gmail.com>
2023-03-03 12:31:05 -0500

    multiple updates

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,36 +1,52 @@
 import React from 'react'
 import { useState, useEffect} from 'react'
 import '../styles/Nav.css'
-import{Link, Routes, Route} from 'react-router-dom'
+import{Link} from 'react-router-dom'
 
 const Nav = () => {
-  const [menu, setMenu] = useState([])
+  const [menu, setMenu] = useState(false)
 
-  useEffect(()=>{
-
-  const menuSetter = false
-  setMenu(menuSetter)
-
-  }, [])
+  
   
 
-  return (
+return (
 <nav className='nav' id="nav">
-<div className='logo'><a href='/'>
+
+
+
+
+<ul>
+<li className='logo'><Link to='/'>
   <img src ="/logo_transparent.png" alt="logo" />
-</a></div>
-    
-{/* <ul>
-<li><button className='nav-btn' onClick={()=>setMenu(true)}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="12" height="12"><path fill="none" d="M0 0h24v24H0z"/><path d="M3 4h18v2H3V4zm0 7h18v2H3v-2zm0 7h18v2H3v-2z"/></svg></button>
-{menu? */}
+  </Link></li>
+  <ul class="nav-list">
+  <li className='nav-item'><Link to="/">Home</Link></li>
+  <li className='nav-item'><Link  to="/news">News</Link></li>
+  <li className='nav-item'><Link  to="/contact">Contact</Link></li>
+  </ul>
+  </ul>
 
-<div class="nav-list">
-  <Link className='nav-item' to="/">Home</Link>
-  <Link className='nav-item' to="/news">News</Link>
-  <Link className='nav-item' to="/contact">Contact</Link>
-  </div>
+{/* Mobile */}
+
+<ul className='nav-list-mobile'>
+<button className='nav-mobile-menu-btn' onClick={()=>setMenu(true)}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16"><path fill="none" d="M0 0h24v24H0z"/><path d="M3 4h18v2H3V4zm0 7h18v2H3v-2zm0 7h18v2H3v-2z"/></svg></button>
+{menu? 
 
 
+
+  <ul>
+
+  <ul className="nav-list-mobile-items">
+  <button className='nav-mobile-close-btn' onClick={()=>setMenu(false)}>X</button>
+  <li className='nav-list-mobile-item'><Link to="/">Home</Link></li>
+  <li className='nav-list-mobile-item'><Link  to="/news">News</Link></li>
+  <li className='nav-list-mobile-item'><Link  to="/contact">Contact</Link></li>
+  
+  </ul>
+  
+  </ul>:null
+}
+</ul>
     </nav>
   )
 }
