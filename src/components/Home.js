e7a50fa7d4b3df841399c63e4f6c86c9eae4d217
eleshinlomo <seun.olatunji2@gmail.com>
2023-03-13 15:35:20 -0400

    updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,12 @@ import '../styles/App.css'
 import Nav from './Nav'
 import News from './News'
 import Footer from './Footer'
-import Saas from './Saas'
 import Header from './Header'
 import Modal from './Modal'
 import Newstwo from './Newstwo'
 import Newsthree from './Newsthree'
-import Feedback from './Contact'
 import {Route, Routes} from 'react-router-dom'
-import Newsaas from './Projects'
+
 
 const Home = () => {
   return (
@@ -21,7 +19,6 @@ const Home = () => {
     <Header />
     <News />
     <Newstwo />
-    <Saas />
     <Newsthree />
     </>
   )
